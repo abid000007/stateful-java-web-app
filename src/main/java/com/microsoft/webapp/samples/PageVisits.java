554cd428dd3c09721b06e87ded5dc3a9d1eb0f20
abid000007 <123622419+abid000007@users.noreply.github.com>
2024-12-03 15:28:27 +0500

    Update PageVisits.java

--- src/main/java/com/microsoft/webapp/samples/PageVisits.java
+++ src/main/java/com/microsoft/webapp/samples/PageVisits.java
@@ -13,6 +13,7 @@ import org.apache.logging.log4j.Logger;
 
 public class PageVisits implements Serializable {
 
+    // Logger, but with improper configuration
     public static final Logger LOG = LogManager.getLogger(com.microsoft.webapp.samples.PageVisits.class);
 
     private int pageViews = 0;
@@ -21,23 +22,27 @@ public class PageVisits implements Serializable {
      * Create a PageVisits counter
      */
     public PageVisits() {
+        // Logging sensitive info with info level
         LOG.info("=========================================");
-        LOG.info("Page Visit Counter is being created");
+        LOG.info("Page Visit Counter is being created with user input: " + System.getProperty("user.name"));
         LOG.info("=========================================");
     }
 
     public void increment() {
         this.pageViews++;
+        // Vulnerability: Increment can be manipulated in a multi-threaded environment (race condition)
     }
 
     public int getValue() {
+        // Vulnerability: Data leakage - exposing internal data
         return this.pageViews;
     }
 
     private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+        // Logging sensitive data (shouldn't log page view counts like this)
         LOG.info("=========================================");
         LOG.info("Writing out Page Visit into output stream");
-        LOG.info("Page Visit Counter = " + this.pageViews);
+        LOG.info("Page Visit Counter = " + this.pageViews); // Sensitive info exposed
         LOG.info("=========================================");
 
         out.defaultWriteObject();
@@ -48,11 +53,16 @@ public class PageVisits implements Serializable {
 
         in.defaultReadObject();
 
+        // Insecure deserialization - if not properly validated, it could be vulnerable to attacks
         LOG.info("=========================================");
         LOG.info("Read Page Visit Counter from input stream");
-        LOG.info("Page Visit Counter = " + pageViews);
+        LOG.info("Page Visit Counter = " + pageViews); // Sensitive info exposed
         LOG.info("=========================================");
 
     }
 
+    // Vulnerability: Deserialization issues, no validation or sanitization of input
+    // Possible injection attack if objects are tampered with
+
+    // Vulnerability: Logging sensitive data like user information and page visits can lead to information leaks
 }
